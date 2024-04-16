80f9c6244e10837480b8a3a4ef7a4cf4311e0b2b
lucianarps <132854975+lucianarps@users.noreply.github.com>
2024-04-15 22:40:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var animation = lottie.loadAnimation({
+        container: document.getElementById('lottie'), // ID do elemento
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: <><script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script><dotlottie-player src="https://lottie.host/da882875-baf2-4496-bd66-a3c634231cf7/FIwX07fuMs.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></dotlottie-player></>
+});
+
